1d5e5840b3fa397779e8fdea3a6ee16858c31592
emct101 <125534289+emct101@users.noreply.github.com>
2023-05-31 05:01:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        return render_template('welcome.html', name=name)
+    return render_template('index.html')
+
