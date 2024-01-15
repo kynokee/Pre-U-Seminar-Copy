5d7185fc09bc777d6444fa0f488dab5ea4443a62
kynokee <125532446+kynokee@users.noreply.github.com>
2024-01-15 06:10:29 +0000

    Enhance Pre-U Seminar Webapp to industry practices

--- main.py
+++ main.py
@@ -4,8 +4,30 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        return render_template('welcome.html', name=name)
-    return render_template('index.html')
+  if request.method == 'POST':
+    name = request.form.get('name')
+    return f"Hello, {name}!"
+  return render_template('index.html')
+
+@app.route('/welcome')
+def page2():
+  return render_template('welcome.html')
+
+@app.route('/creativity')
+def page3():
+  return render_template('creativity.html')
+
+@app.route('/problemsolving')
+def page4():
+  return render_template('problemsolving.html')
+
+@app.route('/comms')
+def page5():
+  return render_template('comms.html')
+
+@app.route('/critical')
+def page6():
+  return render_template('critical.html')
+  
+app.run(host='0.0.0.0', port=81)
 
--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 document.getElementById('submitBtn').addEventListener('click', function() {
-    var nameInput = document.getElementById('nameInput');
-    var name = nameInput.value;
+  var nameInput = document.getElementById('nameInput');
+  var name = nameInput.value;
 
-    if (name !== '') {
-        var welcomeMessage = 'Welcome, ' + name + '!';
-        alert(welcomeMessage);
-        nameInput.value = '';
-    } else {
-        alert('Please enter your name.');
-    }
+  if (name !== '') {
+    var welcomeMessage = 'Welcome, ' + name + '!';
+    alert(welcomeMessage);
+    nameInput.value = '';
+  } else {
+    alert('Please enter your name.');
+  }
 });
